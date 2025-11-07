49cd97bccde03932726bfde099787512e79eeb17
marsil1990 <ma.silveram@gmail.com>
2025-11-07 18:56:16 -0300

    week3

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const env = require("dotenv").config();
 const app = express();
 const static = require("./routes/static");
 const expressLayouts = require("express-ejs-layouts");
+const baseController = require("./controllers/baseController");
+inventoryRoute = require("./routes/inventoryRoute");
 
 /* ***********************
  * View Engine and Templates
@@ -27,9 +29,10 @@ app.use(static);
  * Index Route
  *************************/
 
-app.get("/", function (req, res) {
-  res.render("index", { title: "home" });
-});
+app.get("/", baseController.buildHome);
+
+// Inventory routes
+app.use("/inv", inventoryRoute);
 
 /* ***********************
  * Local Server Information
